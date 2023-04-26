4ab935582132f880b53cef2a00bdfd793cf1520f
Loren Norman <lorennorman@gmail.com>
2023-04-26 11:08:33 -0400

    week 2 wednesday

--- cart.js
+++ cart.js
@@ -1,4 +1,8 @@
-const cartItems = JSON.parse(localStorage?.getItem('cart') || '[]')
+import { store, component } from 'https://cdn.jsdelivr.net/npm/reefjs@12/dist/reef.es.min.js'
+
+const cartItems = store(JSON.parse(localStorage?.getItem('cart') || '[]'))
+
+component('#cart-count', () => cartItems.length)
 
 export const
   isInCart = id => cartItems.indexOf(id) >= 0,
